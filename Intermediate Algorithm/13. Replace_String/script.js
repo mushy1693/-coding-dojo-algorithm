fc513a3230043f05321ce7a73d15b7ff0db6409a
Nelson <mushy1693@live.com>
2016-02-17 12:14:33 -0800

    Intermediate: fixed replace algorithm

--- Intermediate Algorithm/13. Replace_String/script.js
+++ Intermediate Algorithm/13. Replace_String/script.js
@@ -1,19 +1,25 @@
+// replace string function that compares and replaces the word
 function replaceString(search, replace, sentence){
   var word = '';
   var index;
-  for (var i = 0; i < sentence.length; i++) {
-    if (sentence[i] == search[0]) {
-      for (var x = i; x < i + search.length; x++) {
+  for (var i = 0; i < sentence.length; i++) { // loop through the sentence
+    if (sentence[i] == search[0]) { // check to see if the first letter of the search word match the first sited letter
+      for (var x = i; x < i + search.length; x++) { // obtain the string to compare
         word += sentence[x];
       }
-      if(check(search, word) == true){
+      if(check(search, word) == true){  // if check is true run this code block
         index = i;
-        var newSentence = replaceWord(index, search, replace, sentence);
+        var newSentence = replaceWord(index, search, replace, sentence);  // replace the word in the sentence
+        return newSentence;
+      }else{  // if word is not the same empty word counter and continue looping through the sentence
+        word = '';
       }
     }
   }
+  return sentence;
 }
 
+// compare the replace word and the word found in the sentence that might be a match
 function check(search, word){
   for (var i = 0; i < search.length; i++) {
     if (search[i] != word[i]) {
@@ -23,10 +29,19 @@ function check(search, word){
   return true;
 }
 
+// Replace the word in the sentence
 function replaceWord(index, search, replace, sentence){
-  console.log(index);
-  console.log(replace);
-  console.log(sentence);
+  var newSentence = '';
+  for (var i = 0; i < sentence.length; i++) { // loop through the sentence
+    if (i < index || i > index + search.length - 1) { // save all the words on the left and right side of the word being replaced
+      newSentence += sentence[i];
+    }
+    if (i == index) { // if the index is the first letter of the search word in the sentence attach the replace word
+      newSentence += replace;
+    }
+  }
+  return newSentence;
 }
 
-replaceString("Microsoft", "CodingDojo", "Visit Microsoft!");
+var newSentence = replaceString("Microsoft", "CodingDojo", "Visit Me Microsoft!");
+console.log(newSentence);
