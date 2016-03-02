7e590ce4899fcd4933dce4ebad8e16e1200ba947
Nelson <mushy1693@live.com>
2016-03-01 19:58:55 -0800

    Advance: binary string (add getAllBinary function)

--- Advance Algorithm/6. Binary string/script.js
+++ Advance Algorithm/6. Binary string/script.js
@@ -1,40 +1,47 @@
-function getPower(number){
+function getPower(number){  // get the power of a number
   return Math.pow(2,number);
 }
 
-function generateBinary(number){
-  var binaryString = [];
+function generateBinary(number, power){
+  var item = '';
+  var num = number;
+  while(num != 0){  // store binary into num
+    item += (num % 2);
+    num = Math.floor(num / 2);
+  }
+  if (num == 0) { // attach extra 0's to the binary if its shorter than the power
+    while (item.length < power) {
+      item += '0';
+    }
+  }
+  return item;  // return the binary number
+}
 
-  return binaryString;
+function getAllZero(number){
+  var allZero = '';
+  for (var i = 0; i < number; i++) {
+    allZero += '0';
+  }
+  return allZero;
+}
+
+function getAllBinary(binaryArray, number){
+  var allBinary = binaryArray;
+  for (var i = 1; i < getPower(number); i++) {  // get all the binary
+    allBinary.push(generateBinary(i, number));
+  }
+  return binaryArray;
 }
 
 function binaryString(number){
-  /*
-  console.log(8 % 2);
-  console.log(7 % 2);
-  console.log(6 % 2);
-  console.log(5 % 2);
-  console.log(4 % 2);
-  console.log(3 % 2);
-  console.log(2 % 2);
-  console.log(1 % 2);
-  console.log();
-  console.log(Math.floor(8 / 2));
-  console.log(Math.floor(7 / 2));
-  console.log(Math.floor(6 / 2));
-  console.log(Math.floor(5 / 2));
-  console.log(Math.floor(4 / 2));
-  console.log(Math.floor(3 / 2));
-  console.log(Math.floor(2 / 2));
-  console.log(Math.floor(1 / 2));
-  */
+
   var allBinary = [];
-  var totalPower = getPower(number);
-  for (var i = 1; i <= totalPower; i++) {
-    allBinary.push(generateBinary(i));
-  }
+
+  allBinary.push(getAllZero(number));  // push the binary with all zero into the array
+  allBinary = getAllBinary(allBinary, number);  // get all the binary
+
   console.log(allBinary);
 }
 
-var newArray = binaryString(1);
+var newArray = binaryString(4);
 console.log(newArray);
